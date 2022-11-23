969305e46fb68841d42d6d45379a10e258a5a80d
Przemysław Bukowski <bukowski.przemyslaw.1988@gmail.com>
2022-11-23 16:51:14 +0100

    Initial commit

---
+++ src/main/java/com/bukowskiprzemyslaw/tutorisaweb/TutorIsaWebApplication.java
@@ -0,0 +1,13 @@
+package com.bukowskiprzemyslaw.tutorisaweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorIsaWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TutorIsaWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bukowskiprzemyslaw/tutorisaweb/TutorIsaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bukowskiprzemyslaw.tutorisaweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorIsaWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
