5b6f1f86bb1a79c2555ed9d90c5e98e1581a0662
Przemysław Bukowski <bukowski.przemyslaw.1988@gmail.com>
2022-11-23 18:23:31 +0100

    Add entity skeleton

---
+++ src/main/java/com/bukowskiprzemyslaw/tutorisaweb/entity/tutor/Tutor.java
@@ -0,0 +1,10 @@
+package com.bukowskiprzemyslaw.tutorisaweb.entity.tutor;
+
+public class Tutor {
+
+    private Long tutorId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+}
---
+++ src/main/java/com/bukowskiprzemyslaw/tutorisaweb/entity/user/User.java
@@ -0,0 +1,6 @@
+package com.bukowskiprzemyslaw.tutorisaweb.entity.user;
+
+public class User {
+    private long userId;
+    private String ipAddress;
+}
