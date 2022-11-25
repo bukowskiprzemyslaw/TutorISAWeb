5d04ecec5fa482d13186e319880a447c377025ee
Przemysław Bukowski <bukowski.przemyslaw.1988@gmail.com>
2022-11-25 19:07:03 +0100

    Add tutor skeleton

--- src/main/java/com/bukowskiprzemyslaw/tutorisaweb/controller/PageController.java
+++ src/main/java/com/bukowskiprzemyslaw/tutorisaweb/controller/PageController.java
@@ -1,9 +1,13 @@
 package com.bukowskiprzemyslaw.tutorisaweb.controller;
 
+import com.bukowskiprzemyslaw.tutorisaweb.entity.Tutor;
 import com.bukowskiprzemyslaw.tutorisaweb.repository.TutorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -23,6 +27,16 @@ public class PageController {
         ModelAndView mav = new ModelAndView("tutorlist");
         mav.addObject("tutors", tutorRepository.findAll());
         return mav;
+    }
+
+    @PostMapping("/newtutor")
+    public String addTutor(Tutor tutor, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            return "add-tutor";
+        }
+
+        tutorRepository.save(tutor);
+        return "redirect:/tutorlist";
     }
 
 }
--- src/main/java/com/bukowskiprzemyslaw/tutorisaweb/repository/TutorRepository.java
+++ src/main/java/com/bukowskiprzemyslaw/tutorisaweb/repository/TutorRepository.java
@@ -2,6 +2,7 @@ package com.bukowskiprzemyslaw.tutorisaweb.repository;
 
 import com.bukowskiprzemyslaw.tutorisaweb.entity.Tutor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
