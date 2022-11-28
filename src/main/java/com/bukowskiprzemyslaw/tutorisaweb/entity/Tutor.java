c13ae7b2f27fb0b5ad630bb3b2e40dba7e449427
Przemysław Bukowski <bukowski.przemyslaw.1988@gmail.com>
2022-11-28 19:00:28 +0100

    Added update-tutor template, deleted unused methods, change application properties

--- src/main/java/com/bukowskiprzemyslaw/tutorisaweb/entity/Tutor.java
+++ src/main/java/com/bukowskiprzemyslaw/tutorisaweb/entity/Tutor.java
@@ -23,45 +23,4 @@ public class Tutor {
     private String lastName;
     private String email;
     private String phoneNumber;
-
-
-    public Long getTutorId() {
-        return tutorId;
-    }
-
-    public void setTutorId(Long tutorId) {
-        this.tutorId = tutorId;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
 }
