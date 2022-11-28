8f06f4858cd619479389a8fd815e3016ad7c0e86
Przemysław Bukowski <bukowski.przemyslaw.1988@gmail.com>
2022-11-28 20:50:06 +0100

    added update and delete functionality skeleton

--- src/main/java/com/bukowskiprzemyslaw/tutorisaweb/controller/PageController.java
+++ src/main/java/com/bukowskiprzemyslaw/tutorisaweb/controller/PageController.java
@@ -2,12 +2,15 @@ package com.bukowskiprzemyslaw.tutorisaweb.controller;
 
 import com.bukowskiprzemyslaw.tutorisaweb.entity.Tutor;
 import com.bukowskiprzemyslaw.tutorisaweb.repository.TutorRepository;
+import com.bukowskiprzemyslaw.tutorisaweb.service.TutorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -30,7 +33,7 @@ public class PageController {
     }
 
     @GetMapping("/newtutor")
-    public String newTutorForm (Model model) {
+    public String newTutorForm(Model model) {
         model.addAttribute("tutor", new Tutor());
         return "add-tutor";
     }
@@ -38,6 +41,20 @@ public class PageController {
     @PostMapping("/newtutor")
     public String addTutor(Tutor tutor) {
         tutorRepository.save(tutor);
+        return "redirect:/tutorlist";
+    }
+
+    @GetMapping("/updatetutor")
+    public ModelAndView updateTutor(@RequestParam Long tutorId) {
+        ModelAndView mav = new ModelAndView("add-tutor");
+        Tutor tutor = tutorRepository.findById(tutorId).get();
+        mav.addObject("tutor", tutor);
+        return mav;
+    }
+
+    @GetMapping("/deletetutor")
+    public String deleteTutor(@RequestParam Long tutorId) {
+        tutorRepository.deleteById(tutorId);
         return "redirect:/tutorlist";
     }
 
