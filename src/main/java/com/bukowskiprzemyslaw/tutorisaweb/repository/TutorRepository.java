c0666fecf01926e16b7be2035e575649f470d274
Przemysław Bukowski <bukowski.przemyslaw.1988@gmail.com>
2022-12-06 22:16:30 +0100

    Updated TutorRepository

--- src/main/java/com/bukowskiprzemyslaw/tutorisaweb/repository/TutorRepository.java
+++ src/main/java/com/bukowskiprzemyslaw/tutorisaweb/repository/TutorRepository.java
@@ -1,11 +1,10 @@
 package com.bukowskiprzemyslaw.tutorisaweb.repository;
 
 import com.bukowskiprzemyslaw.tutorisaweb.entity.Tutor;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TutorRepository extends JpaRepository<Tutor, Long> {
+public interface TutorRepository extends CrudRepository<Tutor, Long> {
 }
 
