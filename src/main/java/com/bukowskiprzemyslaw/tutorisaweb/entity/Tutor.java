b04b1a330cb6f4550e4508486c8d58cd04ae3cf4
Przemysław Bukowski <bukowski.przemyslaw.1988@gmail.com>
2022-12-06 22:14:09 +0100

    Updated Tutor entity

--- src/main/java/com/bukowskiprzemyslaw/tutorisaweb/entity/Tutor.java
+++ src/main/java/com/bukowskiprzemyslaw/tutorisaweb/entity/Tutor.java
@@ -3,24 +3,92 @@ package com.bukowskiprzemyslaw.tutorisaweb.entity;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
 
 @Entity
-@Table(name = "tbl_tutors")
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+
 public class Tutor {
 
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
-    private Long tutorId;
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotBlank(message = "Proszę podać imię trenera")
     private String firstName;
+
+    @NotBlank(message = "Proszę podać nazwisko trenera")
     private String lastName;
+
+    @Email(message = "Proszę podać adres e-mail w formacie xxx@xx.xx")
     private String email;
+
+    @Min(9)
+    @Max(11)
+    @NotBlank(message = "Proszę podać telefon składający się z 9-11 cyfr")
     private String phoneNumber;
+
+    public Tutor(Long id, String firstName, String lastName, String email, String phoneNumber) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Tutor{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                '}';
+    }
 }
+
