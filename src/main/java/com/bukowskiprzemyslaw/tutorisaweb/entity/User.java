be8c13af76896863533ccc94b4277dcffd1fb989
Przemysław Bukowski <bukowski.przemyslaw.1988@gmail.com>
2022-12-10 19:37:51 +0100

    [Security] Added User and role entities

---
+++ src/main/java/com/bukowskiprzemyslaw/tutorisaweb/entity/User.java
@@ -0,0 +1,57 @@
+package com.bukowskiprzemyslaw.tutorisaweb.entity;
+
+import javax.persistence.*;
+import java.util.Collection;
+
+@Entity
+@Table(name = "user", uniqueConstraints = @UniqueConstraint(columnNames = "login"))
+public class User  {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String login;
+
+    private String password;
+
+    private Collection<Role> roles;
+
+    public User(String login, String password, Collection<Role> roles) {
+        this.login = login;
+        this.password = password;
+        this.roles = roles;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Collection<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Collection<Role> roles) {
+        this.roles = roles;
+    }
+}
