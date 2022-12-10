6dd6b46bcb3a57922174f79e4d3735c5a4e18bb9
Przemysław Bukowski <bukowski.przemyslaw.1988@gmail.com>
2022-12-10 19:54:48 +0100

    [Security] Linked Role and User entities by @ManyToMany

--- src/main/java/com/bukowskiprzemyslaw/tutorisaweb/entity/User.java
+++ src/main/java/com/bukowskiprzemyslaw/tutorisaweb/entity/User.java
@@ -15,6 +15,14 @@ public class User  {
 
     private String password;
 
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = @JoinColumn(
+                    name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(
+                    name = "role_id", referencedColumnName = "id"))
+
     private Collection<Role> roles;
 
     public User(String login, String password, Collection<Role> roles) {
