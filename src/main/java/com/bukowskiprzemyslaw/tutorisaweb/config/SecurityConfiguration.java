a41913d4a01e7fcb040c76d50c1dfca1babf7fed
Bukowski <bukowski.przemyslaw.1988@gmail.com>
2022-12-25 19:50:47 +0100

    [Security] Added BCrypt password encoder

--- src/main/java/com/bukowskiprzemyslaw/tutorisaweb/config/SecurityConfiguration.java
+++ src/main/java/com/bukowskiprzemyslaw/tutorisaweb/config/SecurityConfiguration.java
@@ -1,14 +1,21 @@
 package com.bukowskiprzemyslaw.tutorisaweb.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
