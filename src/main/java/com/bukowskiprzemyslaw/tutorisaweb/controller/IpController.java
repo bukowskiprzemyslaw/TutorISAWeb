7a0c5d50b3847b00299ead1edfd52afe110900c6
Bukowski <bukowski.przemyslaw.1988@gmail.com>
2023-04-12 20:35:51 +0200

    Fix to IpController which display proper IP Address

--- src/main/java/com/bukowskiprzemyslaw/tutorisaweb/controller/IpController.java
+++ src/main/java/com/bukowskiprzemyslaw/tutorisaweb/controller/IpController.java
@@ -6,14 +6,31 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.UnknownHostException;
+
 @Controller
-public class IpController
-{
+public class IpController {
 
     @GetMapping("ip")
-    public String getIpAddress(HttpServletRequest request, Model model) {
+    public String getIpAddress(HttpServletRequest request, Model model) throws UnknownHostException, MalformedURLException {
         String ipAddress = request.getRemoteAddr();
+
+        if (ipAddress.equalsIgnoreCase("0:0:0:0:0:0:0:1")) {
+            try (
+                    BufferedReader br = new BufferedReader(new InputStreamReader(new URL("https://checkip.amazonaws.com").openStream()));
+            ) {
+                ipAddress = br.readLine();
+            } catch (Exception e) {
+            }
+        }
+
         model.addAttribute("ipAddress", ipAddress);
         return "ip-address";
     }
+
 }
